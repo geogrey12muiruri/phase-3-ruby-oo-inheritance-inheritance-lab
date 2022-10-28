51f46455ba9d027653cdd5670bc83c33f907767c
geogrey12muiruri <greggambugua@gmail.com>
2022-10-28 16:34:04 +0300

    commit tree f7c4bae21c0c3ea008020793e8d4c40942917a3f

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+    class Student < User
+
+        attr_accessor :knowledge
+      
+        def initialize 
+          super
+          @knowledge=[]
+        end
+      
+        def learn(str)
+          @knowledge << str
+        end
+      end
 
 end
+student = User.new("greg", "mbugua")
